2595a1ca76b21f2233da58ab690502bdcb678d7c
alexnegoi09 <48884734+alexnegoi09@users.noreply.github.com>
2020-11-25 20:22:30 +0200

    Add files via upload

---
+++ websites/easybank-landing-page/js/main.js
@@ -0,0 +1,9 @@
+const menuBtn = document.querySelector(".menu-icon");
+const mobileMenu = document.querySelector(".menu-items-small");
+
+menuBtn.addEventListener("click", function() {
+    mobileMenu.classList.toggle("menu-items-small-hide");
+
+})
+
+

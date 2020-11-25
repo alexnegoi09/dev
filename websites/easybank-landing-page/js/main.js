1bc2035d1934e4ef91636c5d5109eda0f7800e67
alexnegoi09 <48884734+alexnegoi09@users.noreply.github.com>
2020-11-26 00:42:28 +0200

    Delete main.js

--- websites/easybank-landing-page/js/main.js
+++
@@ -1,9 +0,0 @@
-const menuBtn = document.querySelector(".menu-icon");
-const mobileMenu = document.querySelector(".menu-items-small");
-
-menuBtn.addEventListener("click", function() {
-    mobileMenu.classList.toggle("menu-items-small-hide");
-
-})
-
-

46496c487d1f105b3ed9afd322fd6e175c583049
alexnegoi09 <48884734+alexnegoi09@users.noreply.github.com>
2020-11-30 11:02:14 +0200

    Add files via upload

--- websites/easybank-landing-page/js/main.js
+++ websites/easybank-landing-page/js/main.js
@@ -4,8 +4,17 @@
 
 const menuBtn = document.querySelector(".menu-icon-sm");
 const menu = document.querySelector(".menu-items-sm");
+const menuSmall = document.querySelector(".menu-items-sm");
 
 
 menuBtn.addEventListener("click", function() {
     menu.classList.toggle("menu-items-sm-show");
-})
+});
+
+
+// hide navbar on resize
+
+window.addEventListener("resize", function() {
+   if (screen.availWidth < 250 || screen.availWidth > 1200) { menuSmall.style.display = "none"; } else { menuSmall.style.display = "flex"; }
+
+}) 
